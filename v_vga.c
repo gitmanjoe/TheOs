77198dc9743d8332392b64f5bc22e734b25cbc07
DOOMZOOM <104279962+Link-Games@users.noreply.github.com>
2025-09-19 14:19:58 +0000

    uncommented

--- v_vga.c
+++ v_vga.c
@@ -1,7 +1,8 @@
-#include "v_vga.h"
+#include "p_port.h"
 #include "t_types.h"
+#include "v_vga.h"
 
-/*void enable_cursor(uint8_t cursor_start, uint8_t cursor_end)
+void enable_cursor(uint8_t cursor_start, uint8_t cursor_end)
 {
 	outb(0x3D4, 0x0A);
 	outb(0x3D5, (inb(0x3D5) & 0xC0) | cursor_start);
@@ -15,4 +16,4 @@ void disable_cursor()
 {
 	outb(0x3D4, 0x0A);
 	outb(0x3D5, 0x20);
-}*/
+}
