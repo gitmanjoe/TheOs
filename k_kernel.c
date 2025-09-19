6c928c14693f84de5f98c36e70e59aa3bd348ae0
DOOMZOOM <104279962+Link-Games@users.noreply.github.com>
2025-09-19 13:43:22 +0000

    added suffix in filename

---
+++ k_kernel.c
@@ -0,0 +1,27 @@
+#include <stdint.h>
+#include "types.h"
+
+//TEST CODE!!!!! REMOVE ALL OF THIS AFTER!!!!!!!!!
+
+#define VIDEO_MEMORY ((uint16_t*)0xB8000)
+#define SCREEN_WIDTH 80
+
+void print_char(char c, int x, int y, uint8_t color) {
+    uint16_t *pos = VIDEO_MEMORY + y * SCREEN_WIDTH + x;
+    *pos = (uint16_t)c | ((uint16_t)color << 8);
+}
+
+void kernel() {
+    print_char('P', 0, 0, 0x0F);
+    print_char('E', 1, 0, 0x0F);
+    print_char('N', 2, 0, 0x0F);
+    print_char('I', 3, 0, 0x0F);
+    print_char('S', 4, 0, 0x0F);
+    print_char(' ', 5, 0, 0x0F);
+    print_char('P', 6, 0, 0x0F);
+    print_char('A', 7, 0, 0x0F);
+    print_char('R', 8, 0, 0x0F);
+    print_char('S', 9, 0, 0x0F);
+    print_char('E', 10, 0, 0x0F);
+    print_char('R', 11, 0, 0x0F);
+}
---
+++ t_types.h
@@ -0,0 +1,14 @@
+#ifndef T_TYPES_H
+#define T_TYPES_H
+
+#define uint8_t  unsigned char
+#define uint16_t unsigned short
+#define uint32_t unsigned int
+#define uint64_t unsigned long long
+
+#define int8_t  char
+#define int16_t short
+#define int32_t int
+#define int64_t long long
+
+#endif //T_TYPES_H
