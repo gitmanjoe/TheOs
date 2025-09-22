8e5c03520749dba024c1f86729690a97dba8ac8a
DOOMZOOM <104279962+Link-Games@users.noreply.github.com>
2025-09-22 14:58:05 +0000

    added enum

--- p_port.h
+++ p_port.h
@@ -3,12 +3,23 @@
 
 #include "t_types.h"
 
-//Output on port: put specified byte on specified port 
-// port: RANGE 0-65535; value RANGE 0-255;
+enum ports {
+    COM1 = 0x3F8,
+    COM2 = 0x2F8,
+    COM3 = 0x3E8,
+    COM4 = 0x2E8,
+    COM5 = 0x5F8,
+    COM6 = 0x4F8,
+    COM7 = 0x5E8,
+    COM8 = 0x4E8,
+};
+
+//Output to port
+//port: RANGE 0-65535; value: RANGE 0-255;
 void outb(uint16_t port, uint8_t value);
 
-//Read port: Return value at specified port 
-// port: RANGE 0-65535;
+//Read port
+//port: RANGE 0-65535;
 uint16_t inb(uint32_t port);
 
 #endif //P_PORT_H
